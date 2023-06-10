dbbd81f9031df4b07f4d68ba16b0e41bb742539d
Joy Brar <joybrar2001@gmail.com>
2023-06-10 20:42:49 +0530

    Finished Basic Layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+
+import CustomButton from "./CustomButton"
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] w-full mx-auto flex justify-between items-center px-6 sm:px-16 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="Car Hub Logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+
+        <CustomButton 
+          title="Sign In"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
+    </header>
   )
 }
 
